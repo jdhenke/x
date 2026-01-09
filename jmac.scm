946b7ba31ac9cee1bc87981cbf11ddbb06222618
Joe Henke <joed.henke@gmail.com>
2026-01-08 20:10:01 -0800

    Add jmac fib

--- jmac.scm
+++ jmac.scm
@@ -29,9 +29,41 @@
 (pretty-print (jmac3 '((lambda (a c) (cons a (cons c (quote ())))) b b) (append '((b 52)) env)))
 (newline)
 
-;(display "JMAC 4")
-;(newline)
-;(define jmac4 (jmac3 jmac env))
-;(pretty-print (jmac4 '((lambda (a c) (cons a (cons c (quote ())))) b b) '((b 52))))
-;(newline)
+(define fib
+  (quote
+    ((lambda (y)
+       (y
+         (lambda (fib)
+           (lambda (n)
+             (cond ((< n three) one)
+                   (#t (+ (fib (- n one)) (fib (- n two)))))))))
+     (lambda (f)
+       ((lambda (x)
+          (f (lambda args (apply (x x) args))))
+        (lambda (x)
+          (f (lambda args (apply (x x) args)))))))))
+
+(pretty-print fib)
+(newline)
+
+(define fibenv (append (list 
+                         (list 'one 1)
+                         (list 'two 2)
+                         (list 'three 3)
+                         (list '< <)
+                         (list '- -)
+                         (list '+ +))
+                       env))
+
+(display "JMAC1 - FIB 10")
+(newline)
+(define j1fib (jmac1 fib fibenv))
+(pretty-print (j1fib 10))
+(newline)
+
+(display "JMAC2 - FIB 10")
+(newline)
+(define j2fib (jmac2 fib fibenv))
+(pretty-print (j2fib 10))
+(newline)
 
